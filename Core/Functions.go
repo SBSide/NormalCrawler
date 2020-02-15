453ad3551e7153a983b53d45ade8aecd217af7d5
Side <ivecis@live.co.kr>
2020-02-15 21:53:40 +0900

    0.03 뭐가 있군요!

--- Core/Functions.go
+++ Core/Functions.go
@@ -1,11 +1,25 @@
 package Core
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func SayHello() {
-	fmt.Println("yay ~")
+func SuperAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
-func SayBye() {
-	fmt.Println("Bye ~")
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func LenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("LenAndUpper : DONE")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"main/Core"
+)
 
 func main() {
-	name := 0x5861
+	name := 5861
+	total := Core.SuperAdd(1, 2, 3, 4, 5, 6)
+	totalLength, upperName := Core.LenAndUpper("nyan")
+	ignore, _ := Core.LenAndUpper("stars")
 	fmt.Println(name)
+	fmt.Println(Core.Multiply(5, 25))
+	fmt.Println(totalLength, upperName)
+	fmt.Println(ignore)
+	fmt.Println("SuperAdd : ", total)
 }
