153b2837cdeac839283e27899eaff1a14e6b7ee7
Side <ivecis@live.co.kr>
2020-02-15 11:59:33 +0900

    0.02 뭐가 있군요!

---
+++ Core/Functions.go
@@ -0,0 +1,11 @@
+package Core
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("yay ~")
+}
+
+func SayBye() {
+	fmt.Println("Bye ~")
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World")
+	name := 0x5861
+	fmt.Println(name)
 }
