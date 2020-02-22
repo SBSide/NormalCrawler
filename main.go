ad77c0991ec72627e0cf598023f1313daea09f3b
Side <ivecis@live.co.kr>
2020-02-23 00:07:49 +0900

    0.05 THERE IS SOMETHING

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"main/Core"
 )
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
 	name := 5861
 	totalLength, upperName := Core.LenAndUpper("nyan")
@@ -18,6 +24,19 @@ func main() {
 	b := &a
 	a = 10
 	*b = 20
+	names := []string{"nyan", "nay", "yaong"}
+	names = append(names, "meow")
+	dogs := map[string]string{"dogs": "bark", "name": "shiro"}
+	for key, val := range dogs {
+		println(key, val)
+	}
 	fmt.Println(a, *b)
-
+	fmt.Println(names, dogs)
+	favFood := []string{"pizza", "ramen"}
+	me := person{
+		name:    "KIM",
+		age:     26,
+		favFood: favFood,
+	}
+	fmt.Println(me.name)
 }
