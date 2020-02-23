463d22479695f637f152ee31fa6c68f5a3bedec1
Side <ivecis@live.co.kr>
2020-02-23 17:32:39 +0900

    0.06 THERE IS SOMETHING

--- main.go
+++ main.go
@@ -2,41 +2,40 @@ package main
 
 import (
 	"fmt"
-	"main/Core"
+	"main/banking"
 )
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	name := 5861
-	totalLength, upperName := Core.LenAndUpper("nyan")
-	ignore, _ := Core.LenAndUpper("stars")
-	fmt.Println(name)
-	fmt.Println(Core.Multiply(5, 25))
-	fmt.Println(totalLength, upperName)
-	fmt.Println(ignore)
-	fmt.Println(Core.CanIDrink(18))
-	a := 2
-	b := &a
-	a = 10
-	*b = 20
-	names := []string{"nyan", "nay", "yaong"}
-	names = append(names, "meow")
-	dogs := map[string]string{"dogs": "bark", "name": "shiro"}
-	for key, val := range dogs {
-		println(key, val)
+	/*	name := 5861
+		totalLength, upperName := core.LenAndUpper("nyan")
+		ignore, _ := core.LenAndUpper("stars")
+		fmt.Println(name)
+		fmt.Println(core.Multiply(5, 25))
+		fmt.Println(totalLength, upperName)
+		fmt.Println(ignore)
+		fmt.Println(core.CanIDrink(18))
+		a := 2
+		b := &a
+		a = 10
+		*b = 20
+		names := []string{"nyan", "nay", "yaong"}
+		names = append(names, "meow")
+		dogs := map[string]string{"dogs": "bark", "name": "shiro"}
+		for key, val := range dogs {
+			println(key, val)
+		}
+		fmt.Println(a, *b)
+		fmt.Println(names, dogs)
+		favFood := []string{"pizza", "ramen"}
+		me := person{
+			name:    "KIM",
+			age:     26,
+			favFood: favFood,
+		}
+		fmt.Println(me.name)*/
+	account := banking.Account{
+		Owner:   "Nyan",
+		Balance: 10000,
 	}
-	fmt.Println(a, *b)
-	fmt.Println(names, dogs)
-	favFood := []string{"pizza", "ramen"}
-	me := person{
-		name:    "KIM",
-		age:     26,
-		favFood: favFood,
-	}
-	fmt.Println(me.name)
+	fmt.Println(account)
 }
