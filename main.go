cfd327f725d52412e74fb1cf73ba685f698387a4
Side <ivecis@live.co.kr>
2020-03-10 09:49:58 +0900

    0.15 Implemented csv output function.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 func main() {
 	var jobs []core.ExtractedJob
 	totalpages := core.GetPages()
-	fmt.Println(totalpages)
 	for i := 0; i < totalpages; i++ {
 		extractedJobs := core.GetPage(i)
 		jobs = append(jobs, extractedJobs...)
