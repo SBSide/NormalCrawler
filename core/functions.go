fd9bd8ece706cb3d8145cd6d349c86a2b3bcd17b
Side <ivecis@live.co.kr>
2020-03-11 16:38:20 +0900

    0.15 Implemented csv output function.

--- core/functions.go
+++ core/functions.go
@@ -26,26 +26,9 @@ type ExtractedJob struct {
 	Summary  string
 }
 
-func WriteJobs(jobs []ExtractedJob) {
-	file, err := os.Create("jobs.csv")
-	checkErr(err)
-	w := csv.NewWriter(file)
-	defer w.Flush()
-
-	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
-
-	wErr := w.Write(headers)
-	checkErr(wErr)
-
-	for _, job := range jobs {
-		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.Id, job.Title, job.Location, job.Salary, job.Summary}
-		jwErr := w.Write(jobSlice)
-		checkErr(jwErr)
-	}
-}
-
-func GetPage(page int) []ExtractedJob {
+func GetPage(page int, mainC chan<- []ExtractedJob) {
 	var jobs []ExtractedJob
+	c := make(chan ExtractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting ", pageURL)
 	res, err := http.Get(pageURL)
@@ -60,19 +43,22 @@ func GetPage(page int) []ExtractedJob {
 	searchCards := doc.Find(".jobsearch-SerpJobCard")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
+		go extractJob(card, c)
 	})
-	return jobs
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainC <- jobs
 }
 
-func extractJob(card *goquery.Selection) ExtractedJob {
+func extractJob(card *goquery.Selection, c chan<- ExtractedJob) {
 	id, _ := card.Attr("data-jk")
 	title := cleanString(card.Find(".title>a").Text())
 	location := cleanString(card.Find(".sjcl").Text())
 	salary := cleanString(card.Find(".salaryText").Text())
 	summary := cleanString(card.Find(".summary").Text())
-	return ExtractedJob{
+	c <- ExtractedJob{
 		id,
 		title,
 		location,
@@ -100,6 +86,25 @@ func GetPages() int {
 		pages = s.Find("a").Length()
 	})
 	return pages
+}
+
+func WriteJobs(jobs []ExtractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	//TODO: go routine 으로 전환. Write 가 동시에 이루어지도록 할 것.
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.Id, job.Title, job.Location, job.Salary, job.Summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
 }
 
 func checkErr(err error) {
--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	var jobs []core.ExtractedJob
+	c := make(chan []core.ExtractedJob)
 	totalpages := core.GetPages()
 	for i := 0; i < totalpages; i++ {
-		extractedJobs := core.GetPage(i)
+		go core.GetPage(i, c)
+	}
+
+	for i := 0; i < totalpages; i++ {
+		extractedJobs := <-c
 		jobs = append(jobs, extractedJobs...)
 	}
 	core.WriteJobs(jobs)
